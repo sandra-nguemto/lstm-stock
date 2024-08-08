a85168caa83f03bbf102bc9e8612f6032a3a7356
Sandra Nguemto <agn2120@columbia.edu>
2024-08-08 05:05:42 -0400

    add files

---
+++ app.py
@@ -0,0 +1,137 @@
+# pip install numpy
+import numpy as np
+
+# pip install torch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+# pip install alpha_vantage
+from alpha_vantage.timeseries import TimeSeries 
+
+import streamlit as st
+import pickle
+
+
+config = {
+    "alpha_vantage": {
+        "key": "2MNZCKJINQYJHNVB", # Claim your free API key here: https://www.alphavantage.co/support/#api-key
+        "symbol": "AAPL",
+        "outputsize": "full",
+        "key_adjusted_close": "4. close",
+    },
+    "data": {
+        "window_size": 20,
+        "train_split_size": 0.80,
+    }, 
+    "plots": {
+        "show_plots": True,
+        "xticks_interval": 90,
+        "color_actual": "#001f3f",
+        "color_train": "#3D9970",
+        "color_val": "#0074D9",
+        "color_pred_train": "#3D9970",
+        "color_pred_val": "#0074D9",
+        "color_pred_test": "#FF4136",
+    },
+    "model": {
+        "input_size": 1, # since we are only using 1 feature, close price
+        "num_lstm_layers": 2,
+        "lstm_size": 32,
+        "dropout": 0.2,
+    },
+    "training": {
+        "device": "cpu", # "cuda" or "cpu"
+        "batch_size": 64,
+        "num_epoch": 100,
+        "learning_rate": 0.01,
+        "scheduler_step_size": 40,
+    }
+}
+
+
+class Normalizer():
+    def __init__(self):
+        self.mu = None
+        self.sd = None
+
+    def fit_transform(self, x):
+        self.mu = np.mean(x, axis=(0), keepdims=True)
+        self.sd = np.std(x, axis=(0), keepdims=True)
+        normalized_x = (x - self.mu)/self.sd
+        return normalized_x
+
+    def inverse_transform(self, x):
+        return (x*self.sd) + self.mu
+
+# normalize
+scaler = Normalizer()
+
+
+class LSTMModel(nn.Module):
+    def __init__(self, input_size=1, hidden_layer_size=32, num_layers=2, output_size=1, dropout=0.2):
+        super().__init__()
+        self.hidden_layer_size = hidden_layer_size
+
+        self.linear_1 = nn.Linear(input_size, hidden_layer_size)
+        self.relu = nn.ReLU()
+        self.lstm = nn.LSTM(hidden_layer_size, hidden_size=self.hidden_layer_size, num_layers=num_layers, batch_first=True)
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(num_layers*hidden_layer_size, output_size)
+        
+        self.init_weights()
+
+    def init_weights(self):
+        for name, param in self.lstm.named_parameters():
+            if 'bias' in name:
+                 nn.init.constant_(param, 0.0)
+            elif 'weight_ih' in name:
+                 nn.init.kaiming_normal_(param)
+            elif 'weight_hh' in name:
+                 nn.init.orthogonal_(param)
+
+    def forward(self, x):
+        batchsize = x.shape[0]
+
+        # layer 1
+        x = self.linear_1(x)
+        x = self.relu(x)
+        
+        # LSTM layer
+        lstm_out, (h_n, c_n) = self.lstm(x)
+
+        # reshape output from hidden cell into [batch, features] for `linear_2`
+        x = h_n.permute(1, 0, 2).reshape(batchsize, -1) 
+        
+        # layer 2
+        x = self.dropout(x)
+        predictions = self.linear_2(x)
+        return predictions[:,-1]
+
+model = LSTMModel(input_size=config["model"]["input_size"], hidden_layer_size=config["model"]["lstm_size"], num_layers=config["model"]["num_lstm_layers"], output_size=1, dropout=config["model"]["dropout"])
+model = model.to(config["training"]["device"])
+
+
+
+
+# Load your PyTorch model
+model_app = torch.load("model.pth")
+
+# Create the Streamlit app
+st.title("PyTorch Model Deployment")
+
+# Upload an image
+uploaded_file = st.file_uploader("Choose a stock...", type=None)
+if uploaded_file is not None:
+    # Preprocess the image
+    stock = uploaded_file
+    # Perform inference
+    with torch.no_grad():
+        prediction = model(stock)
+        prediction = prediction.cpu().detach().numpy()
+        prediction = scaler.inverse_transform(prediction)[0]
+
+    # Display the results
+    st.write(prediction)
